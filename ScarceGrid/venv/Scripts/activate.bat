2d1a5befbf3db979a6be56eb892845ccc47ae57d
Sambhav Vk <sambhavvk2022@outlook.com>
2026-05-01 22:54:56 +0100

    re organisation

--- ScarceGrid/venv/Scripts/activate.bat
+++ ScarceGrid/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\sambh\projs\Information Retrieval\Scarce-Grid\ScarceGrid\venv
+set VIRTUAL_ENV=C:\Users\sambh\projs\Information Retrieval\Scarce-Grid\ScarceGrid\ScarceGrid\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
